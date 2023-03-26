3125fb45502ebee92a25070bf0f685262176ba34
Ryan Bloom <ryan@ryan-bloom.com>
2023-03-26 10:16:33 -0400

    Add other AI endpoints

--- backend/openaiQuestions.py
+++ backend/openaiQuestions.py
@@ -6,6 +6,7 @@ from typing import Dict, List, Optional
 from pydantic import BaseModel
 import uvicorn
 from fastapi.middleware.cors import CORSMiddleware
+from newspaper import Article
 
 origins = ["*"]
 
@@ -32,31 +33,97 @@ app.add_middleware(
     allow_headers=["*"]
 )
 
-@app.post("/addflash/")
-async def add_flashcard(card: Flashcard):
-    # Get the request body as json
-    flash_cards.append(card)
-    return{"message":"Flashcard added successfully"}
+openai.api_key = os.getenv('OPENAI_API_KEY')
+if openai.api_key is None:
+    print("Warning: no OpenAI API key found. Set the OPENAI_API_KEY environment variable.")
+
+
+def chat_history(prompt, data):
+    return [
+        {
+            "role": "user",
+            "content": prompt
+        },
+        {
+            "role": "assistant",
+            "content": "Yes, I understand the instructions."
+        },
+        {
+            "role": "user",
+            "content": data
+        }
+    ]
+
+def chatgpt(prompt, data):
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=chat_history(prompt, data)
+    )
+    return str(response['choices'][0]['message']['content'])
 
 @app.get("/flashcards/")
 async def get_flashcards():
     return flash_cards
 
-openai.api_key = os.getenv('OPENAI_API_KEY')
-print(openai.api_key)
+@app.post("/add")
+async def add_flashcard(card: Flashcard):
+    flash_cards.append(card)
+    return{"message":"Flashcard added successfully"}
 
-@app.get("/generate/")
-def generate_response(prompts:str):
+dynamic_prompt = """
+I'm going to give you a description of a flashcard. Please respond with text for the front and back of the card, liek this: "front | back".
+Do you understand the instructions?
+"""
+
+def generate_dynamic(prompt):
+    response = chatgpt(dynamic_prompt, prompt)
+    [front, back] = response.split(' | ')
+    new_flashcard = Flashcard(dynamic=True, front=front, back=back)
+    flash_cards.append(new_flashcard)
+    return new_flashcard
+
+@app.get("/flashcards-dynamic")
+async def get_flashcards_dynamic():
+    dynamic_flashcards = []
+    for card in flash_cards:
+        if card.dynamic:
+            dynamic_flashcards.append(generate_dynamic(card.prompt))
+        else:
+            dynamic_flashcards.append(card)
+    return dynamic_flashcards
+
+@app.post("/generate")
+def generate_response(prompt:str):
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
-        messages=[
-            {"role":"user","content":f"{prompts}"}
-        ]
+        messages=chat_history(dynamic_prompt, prompt)
     )
     output_gpt: str = str(response['choices'][0]['message']['content'])
-    data = {"question": prompts, "answer": output_gpt}
-    flash_cards[prompts] = output_gpt
-    return output_gpt
+    [front, back] = output_gpt.split(' | ')
+    new_flashcard = Flashcard(front=front, back=back, dynamic=False)
+    flash_cards.append(new_flashcard)
+    return new_flashcard
+
+generate_url_prompt = """
+I'm going to give you the text of an article.
+Please generate a list of flashcards that covers the same information. Write each card on a new line, using the format "front | back". Do not add anything else.
+Do you understand the instructions?
+"""
+
+@app.post("/generate-url")
+def generate_url(url:str):
+    article = Article(url)
+    article.download()
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=chat_history(generate_url_prompt, article.text)
+    )
+    output_gpt: str = str(response['choices'][0]['message']['content'])
+    new_cards = []
+    for line in output_gpt.split('\n'):
+        [front, back] = line.split(' | ')
+        new_cards.append(Flashcard(front=front, back=back))
+    return new_cards
 
 synthesis_prompt = """
 Here is a list of flashcards that I need to study, formatted as front->back.
@@ -64,25 +131,24 @@ Please generate a different list of flashcards that covers the same information.
 Each new flashcard must correspond to one or two old flashcards, with the number(s) in a comma-separated list before the question.
 You may change the structure of the cards to add variety. For instance, you could turn "What year was the Declaration of Independence signed?->1776" into "What important event happened in 1776?->The Declaration of Independence was signed." However, do not introduce new information that wasn't in the original list. All information used in the new flashcards must be somewhere in the list of old flashcards.
 If two cards are closely related, you may combine them into a new one that covers the same information. For example, you could combine "WW2 began in 1939" and "WW2 ended in 1945" into "WW2 lasted 6 years." Do not combine unrelated cards.
+Do you understand the instructions?
 """
 
-@app.get("/synthesize/")
+@app.get("/synthesize")
 def synthesize():
-    message = synthesis_prompt + '\n\n' + '\n'.join([f"({i+1}) {card.front}->{card.back}" for i, card in enumerate(flash_cards) if not card.dynamic])
+    message = '\n'.join([f"{i+1} | {card.front} | {card.back}" for i, card in enumerate(flash_cards) if not card.dynamic])
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
-        messages=[
-            {"role":"user","content":message}
-        ]
+        messages=chat_history(synthesis_prompt, message)
     )
     output_gpt: str = str(response['choices'][0]['message']['content'])
     lines = output_gpt.split('\n')
     synthesized_cards = []
     for line in lines:
-        [front, back] = line.split('->')
+        [numbers, front, back] = line.split(' | ')
+        numbers = [int(n) for n in numbers.split(', ')]
         synthesized_cards.append(Flashcard(front=front, back=back))
     return synthesized_cards
      
-
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=os.getenv('PORT', 8000))
